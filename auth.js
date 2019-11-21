187542fee29b09229857d91bad3006b933ddbd56
Marc <MarcGamesons@users.noreply.github.com>
2019-11-21 05:22:00 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,3 +2,4 @@ var currentHref = window.location.href;
 var str = currentHref.split("&");
 var accessToken = str[0].split("=");
 localStorage.setItem("accesstoken", accessToken[1]);
+window.close();
